a7acdb8a056fd93f4f518cd3ccead53b19c71457
Simyian1 <simonlittle2005@gmail.com>
2025-04-24 02:39:00 -0700

    Finished adding multithreading for sudoku validation

--- sudoku.c
+++ sudoku.c
@@ -15,12 +15,16 @@
 // If complete, a puzzle is valid if all rows/columns/boxes have numbers from 1
 // to psize For incomplete puzzles, we cannot say anything about validity
 
+void deleteSudokuPuzzle(int psize, int **grid);
+
 /* structure for passing data to threads */
 typedef struct {
   int row;
   int column;
   int psize;
   int** grid;
+  int lenGrid;
+  int** validGrid;
 } parameters;
 
 bool validNum(int num)
@@ -38,8 +42,13 @@ bool existsInArray(int num, int* arr, int size)
   return false;
 }
 
-bool checkRow(int row, int psize, int **grid)
+void* checkRow(void* args)
 {
+  parameters* data = (parameters*)args;
+  int row = data->row;
+  int psize = data->psize;
+  int** grid = data->grid;
+  int** validGrid = data->validGrid;
    //create amd initialize mem buffer
   int size = psize + 1;
   int* nums = (int *)malloc((size) * sizeof(int));
@@ -50,20 +59,30 @@ bool checkRow(int row, int psize, int **grid)
   //go through each collumn in the row
   for(int i = 1; i < size; i++)
   {
-    if(existsInArray(grid[row][i], nums, size))
-    {
-      return false;
-    }
+    if(!existsInArray(grid[row][i], nums, size) && validGrid[row][i] != -1)
+      {
+        validGrid[row][i] = 1;
+      }
+      else
+      {
+        validGrid[row][i] = -1;
+      }
     nums[i] = grid[row][i];
   }
   //free
+  free(data);
   free(nums);
-  return true;
+  pthread_exit(NULL);
 }
 
 
-bool checkCol(int col, int psize, int **grid)
+void* checkCol(void* args)
 {
+  parameters* data = (parameters*)args;
+  int col = data->column;
+  int psize = data->psize;
+  int** grid = data->grid;
+  int** validGrid = data->validGrid;
    //create amd initialize mem buffer
   int size = psize + 1;
   int* nums = (int *)malloc((size) * sizeof(int));
@@ -74,19 +93,31 @@ bool checkCol(int col, int psize, int **grid)
   //go through each row in the column
   for(int i = 1; i < size; i++)
   {
-    if(existsInArray(grid[i][col], nums, size))
-    {
-      return false;
-    }
+    if(!existsInArray(grid[i][col], nums, size) && validGrid[i][col] != -1)
+      {
+        validGrid[i][col] = 1;
+      }
+      else
+      {
+        validGrid[i][col] = -1;
+      }
     nums[i] = grid[i][col];
   }
   //free
+  free(data);
   free(nums);
-  return true;
+  pthread_exit(NULL);
 }
 
-bool checkGrid(int row, int col, int psize, int **grid, int lenGrid)
+void* checkGrid(void* args)
 {
+  parameters* data = (parameters*)args;
+  int row = data->row;
+  int col = data->column;
+  int psize = data->psize;
+  int** grid = data->grid;
+  int lenGrid = data->lenGrid;
+  int** validGrid = data->validGrid;
   //create amd initialize mem buffer
   int size = psize + 1;
   int* nums = (int *)malloc((size) * sizeof(int));
@@ -103,62 +134,110 @@ bool checkGrid(int row, int col, int psize, int **grid, int lenGrid)
   {
     for(int j = col; j < endCol && j < size; j++)
     {
-      if(existsInArray(grid[i][j], nums, size))
+      if(!existsInArray(grid[i][j], nums, size) && validGrid[i][j] != -1)
       {
-        return false;
+        validGrid[row][col] = 1;
+      }
+      else
+      {
+        validGrid[row][col] = -1;
       }
       nums[i] = grid[i][j];
     }
   }
-
-  return true;
+  //free
+  free(data);
+  free(nums);
+  pthread_exit(NULL);
 }
 
 void checkPuzzle(int psize, int **grid, bool *complete, bool *valid) {
+  //make valid grid, set every valid pos to 1, if any 0 left invalid
+  int** validGrid = (int **)malloc((psize + 1) * sizeof(int *));
+  for (int row = 1; row <= psize; row++) {
+    validGrid[row] = (int *)malloc((psize + 1) * sizeof(int));
+    for (int col = 1; col <= psize; col++) {
+      validGrid[row][col] = 0;
+    }
+  }
+  
   int lenGrid = sqrt((double)psize);
   int size = psize + 1;
   *valid = true;
   *complete = true;
-  /*
-  parameters* data = (parameters*) malloc(sizeof(parameters));
-  data->row = 1;
-  data->column = 1;
-  data->psize = psize;
-  data->grid = grid;
+  
+  //thread index
+  int threadI = 0;
+  //number of threads is psize * 3 (rows and cols and grid checks),
+  pthread_t* threads = (pthread_t*)malloc((sizeof(pthread_t) * (psize*3)));
 
-  pthread_t = thread;
-  pthread_create(&thread, NULL, func, (void*)data);
-*/
   for(int i = 1; i < size; i++)
   {
+    //data params
+    parameters* dataR = (parameters*) malloc(sizeof(parameters));
+    dataR->row = i;
+    dataR->column = i;
+    dataR->psize = psize;
+    dataR->grid = grid;
+    dataR->validGrid = validGrid;
+
+    parameters* dataC = (parameters*) malloc(sizeof(parameters));
+    dataC->row = i;
+    dataC->column = i;
+    dataC->psize = psize;
+    dataC->grid = grid;
+    dataC->validGrid = validGrid;
+
     //spawn threads to validate each column and row
-    if(!checkCol(i, psize, grid) || !checkRow(i, psize, grid))
-    {
-      *valid = false;
-      break;
-    }
+    pthread_create(&threads[threadI++], NULL, checkCol, (void*)dataC);
+
+    pthread_create(&threads[threadI++], NULL, checkRow, (void*)dataR);
+
     for(int j = 1; j < size; j++)
     {
-      //check complete
-      if(grid[i][j] == 0) *complete = false;
-      if(!validNum(grid[i][j]))
-      {
-        *valid = false;
-        break;
-      }
-      //maybe modulo row or column by lenGrid, and when remainder == 0 then spaen thread for grid validation
+      
+      //maybe modulo row or column by lenGrid, and when remainder == 1 then spawn thread for grid validation
       if(i % lenGrid == 1 && j % lenGrid == 1)
       {
-        if(!checkGrid(i, j, psize, grid, lenGrid))
-        {
-          *valid = false;
-          break;
-        }
+        //data params
+        parameters* dataGrid = (parameters*) malloc(sizeof(parameters));
+        dataGrid->row = i;
+        dataGrid->column = j;
+        dataGrid->psize = psize;
+        dataGrid->grid = grid;
+        dataGrid->validGrid = validGrid;
+        dataGrid->lenGrid = lenGrid;
+
+        //spawn threads to validate each column and row
+        pthread_create(&threads[threadI++], NULL, checkGrid, (void*)dataGrid);
       }
     }
   }
 
-  
+  //join threads
+  for(int i = 0; i < psize * 3; i++)
+  {
+    pthread_join(threads[i], NULL);
+  }
+
+  for(int i = 1; i < size; i++)
+  {
+    for(int j = 1; j < size; j++)
+    {
+      //check complete
+      if(grid[i][j] == 0) *complete = false;
+      //check valid
+      if(validGrid[i][j] == 0 || validGrid[i][j] == -1)
+      {
+        *valid = false;
+      }
+    }
+  }
+
+  free(threads);
+
+  //free valid grid
+  deleteSudokuPuzzle(psize, validGrid);
   
 }
 
